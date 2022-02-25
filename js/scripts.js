5cc8a8cf774040022fe9767370b0088216c3ac30
Deya Card <thedeyacard@gmail.com>
2022-02-25 15:27:42 -0800

    add css style to body

--- js/scripts.js
+++ js/scripts.js
@@ -6,17 +6,17 @@
 function PizzaOrder() {
   this.pizzas = {};
   this.orderTotal = 0;
-  this.pizzaID = 0;
+  this.pizzaId = 0;
 }
 
 PizzaOrder.prototype.assignID = function() {
-  this.pizzaID += 1;
-  return this.pizzaID;
+  this.pizzaId += 1;
+  return this.pizzaId;
 }
 
-PizzaOrder.prototype.addPizza = funtion(pizza) {
+PizzaOrder.prototype.addPizza = function(pizza) {
   pizza.id = this.assignID();
-  this.pizzas[pizza.ID] = pizza;
+  this.pizzas[pizza.Id] = pizza;
   this.orderTotal += pizza.pizzaCost(pizza.toppings, pizza.size);
 }
 PizzaOrder.prototype.findPizza = function(id) {
@@ -55,6 +55,67 @@ Pizza.prototype.pizzaCost = function(toppings, size) {
 
 
 //UI Logic
+
+let pizzaOrder = new PizzaOrder();
+
+function getToppings() {
+  const toppings = [];
+  const checkedToppings = $("input:checkbox[name=ingredient]:checked").each(function() {
+    toppings.push($(this).val());
+  })
+  return toppings;
+}
+
+
+function displayOrder(orderToDisplay){
+  $("#pizzaOrder").show();
+  let pizzasList = $("ul#pizzas");
+  let htmlForPizzas = "";
+  Object.keys(orderToDisplay.pizzas).forEach(function(key){
+    const pizza = orderToDisplay.findPizza(key);
+    htmlForPizzas += "<li id=" + pizza.id + ">" + pizza.size + " pizza - $" + pizza.totalCost.toFixed(2) + "</li>";
+  });
+  pizzasList.html(htmlForPizzas);
+}
+
+function showPizza(pizzaId){
+  const pizza = pizzaOrder.findPizza(pizzaId);
+  $("#show-pizza").toggle();
+  $(".toppings").html(pizza.toppings.join(", "));
+}
+
+function uncheckIngredients(){
+  const checkedToppings = $("input:checkbox[name=ingredient]").each(function(){
+    if($("input:checkbox[name=ingredient]").prop('checked', true)){
+      $("input:checkbox[name=ingredient]").prop('checked', false);
+    }
+  })
+}
+
+$("ul#pizzas").on('click','li', function(){
+  showPizza(this.id);
+})
+
+function addPizzaListeners(){
+  $("ul#pizzas").on('click','li', function(){
+    showPizza(this.id);
+  })
+  }
+
+$(document).ready(function(){
+  addPizzaListeners();
+  $(".sizeColumn, .toppingsColumn, button.submit").hide();
+  $("form#pizzaForm").submit(function(){
+    event.preventDefault();
+    const inputtedSize = $("select#pizzaSize option:selected").val();
+    const inputtedToppings = getToppings();
+    let pizza = new Pizza(inputtedToppings, inputtedSize);
+    pizzaCart.addPizza(pizza);
+    displayCart(pizzaCart);
+    uncheckIngredients();
+    $(".orderTotal").text(parseFloat(pizzaOrder.orderTotal).toFixed(2));
+  })
+})
 
 
 
