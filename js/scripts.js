6383b0ca762705909e3af6e8fef78f2049adbd2a
Deya Card <thedeyacard@gmail.com>
2022-03-01 11:13:38 -0800

    work in progress

--- js/scripts.js
+++ js/scripts.js
@@ -3,43 +3,55 @@
 //Business Logic
 
 function PizzaOrder(size, toppings, price) {
-  this.size = size = [];
-  this.toppings = toppings= [];
-  this.totalPrice = 0;
+  this.size = size;
+  this.toppings = toppings;
+  this.price = 0;
+}
+
+PizzaOrder.prototype.addSize = function() {
+  this.size = 0;
 }
 
 PizzaOrder.prototype.addToppings = function(toppings) {
   this.toppings += 1;
 }
 
-PizzaOrder.prototype.addPizza = function(pizza) {
-  pizza.id = this.assignID();
-  this.pizzas[pizza.Id] = pizza;
-  this.orderTotal += pizza.pizzaCost(pizza.toppings, pizza.size);
-}
-
-
-Pizza.prototype.pizzaCost = function(toppings, size) {
-  size = this.size;
-  toppings = this.toppings;
-  this.totalCost = 0;
-  for(let i = o; i < toppings.length; i ++) {
-    this.totalCost += 1.00;
-  }if(size === 'small') {
-    this.totalCost += 10.00;
-  }else if(size === 'medium') {
-    this.totalCost += 15.00;
+PizzaOrder.prototype.pizzaPrice = function() {
+  this.price = 0;
+  for(let i = 0; i < this.toppings.length; i ++) {
+    this.price += 1.00;
+  }if(this.size === 'small') {
+    this.price += 10.00;
+  }else if(this.size === 'medium') {
+    this.price += 15.00;
   }else {
-    this.totalCost += 20.00;
+    this.price += 20.00;
   }
 }
 
 
 //UI Logic
 
-let pizzaOrder = new PizzaOrder();
+$(document).ready(function() {
+  $("form#orderForm").submit(function(event) {
+    $("#submit").click(function() {
+    let toppings = new array();
+    $('input[name="ingredient"]:checked').each(function() {
+      toppings.push(this.value);
+    });
+    alert("Your order: "+toppings.length+"\n"+"And, it is: "+toppings);
 
-function displayPizzaOrder(pizzaOrderToDisplay) {
-  let 
-}
+    console.log(toppings);
+    let pizzaOrder = new PizzaOrder();
+    let totalPrice = 
+    pizzaOrder.addToppings();
+    pizzaOrder.addSize();
+    pizzaOrder.pizzaCost();
+
+  });
+});
+});
+
+
+
 
